95e2602fe9bb246ff23080ef000db1389dbcb7fb
KrishnoSaha <108836045+KrishnoSaha@users.noreply.github.com>
2022-12-21 22:47:41 +0600

    last

---
+++ controllers/routes/test/index.test.js
@@ -0,0 +1,53 @@
+const app = require('../../app');
+const request = require('supertest');
+
+describe('register', () => {
+  it('returns bad request if first name is missing', async () => {
+    const res = await request(app).post('/register').send({ firstName: 'Jan' });
+
+    expect(res.statusCode).toEqual(201);
+  });
+
+  it('returns bad request if first name is missing', async () => {
+    const res = await request(app)
+      .post('/register')
+      .send({ somethingElse: 'Jan' });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual('you need to pass a firstName');
+  });
+});
+
+describe('order', () => {
+    it('returns bad request if first name is missing', async () => {
+      const res = await request(app).post('/register').send({ firstName: 'Jan' });
+  
+      expect(res.statusCode).toEqual(201);
+    });
+  
+    it('returns bad request if first name is missing', async () => {
+      const res = await request(app)
+        .post('/register')
+        .send({ somethingElse: 'Jan' });
+  
+      expect(res.statusCode).toEqual(400);
+      expect(res.body).toEqual('you need to pass a firstName');
+    });
+  });
+
+  describe('cart', () => {
+    it('returns bad request if first name is missing', async () => {
+      const res = await request(app).post('/register').send({ firstName: 'Jan' });
+  
+      expect(res.statusCode).toEqual(201);
+    });
+  
+    it('returns bad request if first name is missing', async () => {
+      const res = await request(app)
+        .post('/register')
+        .send({ somethingElse: 'Jan' });
+  
+      expect(res.statusCode).toEqual(400);
+      expect(res.body).toEqual('you need to pass a firstName');
+    });
+  });
